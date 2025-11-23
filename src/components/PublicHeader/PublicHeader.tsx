a811626f182f30c57cef4f401e6f7ec5780f0c70
Pedro H. Silva <impedrohenri@gmail.com>
2025-11-23 18:56:04 -0300

    corrige componente publicHeader

--- src/components/PublicHeader/PublicHeader.tsx
+++ src/components/PublicHeader/PublicHeader.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import logo from "../../../public/assets/ml_logo.png";
+import logo from "../../../public/static/images/logo/ml_logo.svg";
+import Link from "next/link";
 
 export default function PublicHeader() {
   return (
     <div className="flex items-center px-20 bg-(--primary-yellow) h-14">
-      <Image src={logo} alt="logo" height={40} />
+      <Link href='/'><Image src={logo} alt="logo" height={40} /></Link>
     </div>
   );
 }
