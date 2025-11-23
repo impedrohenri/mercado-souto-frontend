04633ae904792a23cc259055a9540acfc90f26e1
Pedro H. Silva <impedrohenri@gmail.com>
2025-11-23 15:57:07 -0300

    cria layout do componente header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Header from "@/components/header/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,10 @@
 
-import { ButtonHTMLAttributes } from 'react'
+import React, { ButtonHTMLAttributes } from 'react'
 import styles from './Button.module.css'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
     variant: "primary" | "secondary",
-    children: string
+    children: React.ReactNode
 }
 
 export default function Button({ children, ...props}:ButtonProps) {
