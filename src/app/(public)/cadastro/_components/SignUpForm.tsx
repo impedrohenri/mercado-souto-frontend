bb0f596ef9a982e6bc64dd0b88a0559c63b7ba7e
impedrohenri <impedrohenri@gmail.com>
2025-12-08 02:03:51 -0300

    integra cadastro de cliente

--- src/app/(public)/cadastro/_components/SignUpForm.tsx
+++ src/app/(public)/cadastro/_components/SignUpForm.tsx
@@ -9,11 +9,18 @@ import { Field, FieldError, FieldLabel } from '@/components/ui/field';
 import { FieldInput } from '@/components/ui/input';
 import { cpfMask, phoneMask } from '@/utils/inputMasks';
 import Link from 'next/link';
-
+import { URL_API } from '@/api/index.routes';
+import axios from 'axios';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { toast } from "sonner"
+import { Toaster } from '@/components/ui/sonner';
 
 export default function SignUpForm() {
 
-  const {control, handleSubmit} = useForm<SignUpFormSchema>({
+  const router = useRouter();
+
+  const {control, handleSubmit, setError} = useForm<SignUpFormSchema>({
     resolver: zodResolver(signUpFormSchema),
     defaultValues: {
       email: "",
@@ -25,22 +32,72 @@ export default function SignUpForm() {
     }
   });
 
-  
-  const onHandleSubmit = (data: any) => {
-    console.log(data)
+  const [isLoading, setIsLoading] = useState(false);
+  const [apiError, setApiError] = useState<string | null>(null);
 
-    fetch('http://localhost:3000/users', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
+  const onHandleSubmit = async (data: SignUpFormSchema) => {
+    setIsLoading(true)
+    setApiError(null)
+
+    try {
+      const payload = {
+        name: data.name,
+        email: data.email,
+        cpf: data.cpf,
+        phone: data.phone,
+        password: data.password,
       }
-    })
-  }
 
+      const response = await axios.post(`${URL_API}/client`, payload);
+      const userData = response.data;
+
+      localStorage.setItem('user@clientId', userData.clientId);
+      localStorage.setItem('user@token', userData.token);
+      localStorage.setItem('user@roles', JSON.stringify(userData.roles));
+
+
+      toast.success("Cadastro realizado com sucesso!");
+
+      setTimeout(()=>{
+        router.push('/');
+      }, 1000)
+
+    } catch (error: any) {
+
+      if (error.response) {
+        setApiError(error.response.data?.message || "Erro ao registrar usuário.")
+      }
+
+      if (error.status === 500 && error.response?.data?.includes("username")) {
+        setError("email", {
+          type: "server",
+          message: "Este e-mail já está cadastrado."
+        })
+        toast.error("Este e-mail já está cadastrado.")
+
+        return
+
+      } else if (error.status === 500 && error.response?.data?.includes("cpf")) {
+        setError("cpf", {
+          type: "server",
+          message: "Este cpf já está cadastrado."
+        })
+        toast.error("Este cpf já está cadastrado.")
+        
+        return
+      }
+
+      setApiError(error.response?.data)
+      toast.error("Erro: " + String(error.message))
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <form noValidate onSubmit={handleSubmit(onHandleSubmit)} className='flex flex-col gap-y-3'>
+      <Toaster position="top-center"/>
+
       <Controller name='email' control={control} render={({field, fieldState}) => (
         <Field data-invalid={fieldState.invalid}>
           <FieldLabel>E-mail</FieldLabel>
@@ -91,7 +148,13 @@ export default function SignUpForm() {
 
       <span className='text-sm py-4'>Ao "Continuar", aceito os <Link href="/termos-e-condicoes">Termos e condições</Link> e autorizo o uso dos meus dados de acordo com a <Link href="/declaracao-de-privacidade">Declaração de privacidade.</Link></span>
 
-      <Button type='submit' variant='primary' className='py-3 mt-2'>Continuar</Button>
+      <Button type='submit' variant='primary' className='py-3 mt-2' disabled={isLoading}>
+        {isLoading ? "Enviando..." : "Continuar"}
+      </Button>
+      <Button variant='secondary' type="button" className="py-3 font-medium" onClick={() => { router.push('/login') }}>
+        Fazer login
+      </Button>
+      
     </form>
   )
 }
