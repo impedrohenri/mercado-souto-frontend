d363bd2b319345d0495e964d52bf7cc2125ea173
impedrohenri <impedrohenri@gmail.com>
2025-12-08 02:25:38 -0300

    integra login de cliente

--- src/app/(public)/login/_components/LoginForm.tsx
+++ src/app/(public)/login/_components/LoginForm.tsx
@@ -1,22 +1,25 @@
 'use client'
 
 import Button from "@/components/Button/Button";
-import Input from "@/components/Input/Input";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
-import z from "zod";
 import { loginFormSchema, LoginFormSchema } from "./LoginFormSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FieldInput } from "@/components/ui/input";
 import Link from "next/link";
+import axios from "axios";
+import { URL_API } from "@/api/index.routes";
+import { useState } from "react";
 
 
 export default function LoginForm() {
 
+  const [isLoading, setIsLoading] =  useState(false);
+
   const router = useRouter();
 
-  const {control, handleSubmit} = useForm<LoginFormSchema>({
+  const {control, handleSubmit, setError} = useForm<LoginFormSchema>({
       resolver: zodResolver(loginFormSchema),
       defaultValues: {
         email: "",
@@ -24,9 +27,35 @@ export default function LoginForm() {
       }
     });
 
-  const onHandleSubmit = (data: any) => {
-    console.log(data)
-  }
+  const onHandleSubmit = async (data: LoginFormSchema) => {
+    setIsLoading(true);
+    try {
+      const res = await axios.post(`${URL_API}/login`, data);
+
+      const userData = res.data
+
+      localStorage.setItem('user@clientId', userData.clientId);
+      localStorage.setItem('user@token', userData.token);
+      localStorage.setItem('user@roles', JSON.stringify(userData.roles));
+
+      router.push('/');
+
+    } catch (error: any) {
+
+      
+      if (error.response?.status === 401) {
+        setError("email", { message: "E-mail ou senha incorretos." });
+        return;
+      }
+
+      
+      setError("email", {
+        message: "Ocorreu um erro ao tentar fazer login. Tente novamente."
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
@@ -53,7 +82,7 @@ export default function LoginForm() {
 
         <Link href="/recuperar-senha" className="text-sm">Esqueceu a senha?</Link>
 
-        <Button variant='primary' type="submit" className="py-3 mt-2 font-bold">Continuar</Button>
+        <Button variant='primary' type="submit" className="py-3 mt-2 font-bold" disabled={isLoading}>{isLoading ? "Enviando..." : "Continuar"}</Button>
         <Button variant='secondary' type="button" className="py-3 font-medium" onClick={() => { router.push('/cadastro') }}>Criar conta</Button>
       </form>
     </>
