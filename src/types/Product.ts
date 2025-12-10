1a66841cd6063998d15e6052d7a99a396d9c886a
impedrohenri <impedrohenri@gmail.com>
2025-12-10 04:48:25 -0300

    adiciona tipo de produto

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,3 +1,21 @@
-export type TProduct = {
-    
+export type TProductResponse = {
+  id: number,
+  seller: {
+    id: number,
+    cnpj: string,
+    sales: number,
+    balance: number
+  },
+  category: {
+    id: number,
+    name: string
+  },
+  title: string,
+  specification: string,
+  description: string,
+  price: number,
+  stock: number,
+  imageURL: [
+    string
+  ]
 }
